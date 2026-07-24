d740b9b919e07504e651f71bbd6de8d1c3107ceb
Vyacheslav <vyacheslav@MacBook-Pro-Vyacheslav.local>
2026-07-24 22:37:51 +0300

    routerWrapper

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,7 +10,6 @@ import (
 	. "payment_api/internal/service"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
@@ -29,20 +28,9 @@ func main() {
 	repo := NewPaymentPg(db)
 	service := NewService(repo)
 	handler := NewHandler(service)
-
 	r := chi.NewRouter()
-
-	r.Use(middleware.Logger)
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world!\n"))
-	})
-
-	r.Post("/payment", handler.HandlePost)
-	r.Get("/payment/{id}", handler.HandleGet)
-	r.Get("/payment", handler.HandleGetAll)
-	r.Delete("/payment/{id}", handler.HandleDelete)
-	r.Put("/payment/{id}", handler.HandleUpdate)
+	routerWrapper := NewRouterWrapper(handler, r)
+	routerWrapper.Map()
 
 	fmt.Println("Listening...")
 	err = http.ListenAndServe(":8080", r)
